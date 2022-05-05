f3acd93463311c19732e695adccf27e407032683
Peter Burnett <peterburnett@catalyst-au.net>
2022-05-05 13:50:13 +1000

    Version bump to force upgrade

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022040800;          // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022042801;          // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018051700;          // Requires this Moodle version.
 $plugin->component = 'antivirus_remote';   // Full name of the plugin (used for diagnostics).
